bff13103290b61c2abd6087e848325af622de197
leonardo <demetrio.vd@hotmail.com>
2024-06-03 09:11:59 -0300

    rebase

--- sms-sender/src/component/SMSTwillioComponent.js
+++ sms-sender/src/component/SMSTwillioComponent.js
@@ -2,15 +2,20 @@ require("dotenv").config({ path: "./config/.env" });
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
+const { sendToRedis } = require("../services/RedisService.js");
 
-const sendSMS = (phone) => {
+const sendSMS = (messageTemplate) => {
   client.messages
     .create({
-      body: "Teste",
-      from: "+16192686314",
-      to: "+5511946334467",
+      body: messageTemplate.message,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: messageTemplate.cellPhone,
     })
-    .then((message) => console.log("Mensagem enviada com sucesso "));
+    .then((message) => console.log("Mensagem enviada com sucesso "))
+    .catch((err) => {
+      console.log("Erro to send message");
+      sendToRedis(messageTemplate);
+    });
 };
 
 module.exports = { sendSMS };
